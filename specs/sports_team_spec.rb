510318caf2b5fbb9f4321dd5551ac96c0f9166e1
Alastair Hood <alastair.hood85@gmail.com>
2017-01-16 16:07:15 +0000

    intermediate 2

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -1,3 +1,53 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../sports_team")
+
+class TestSports_team < Minitest::Test  
+
+  def setup
+    @pottstown = SportsTeam.new("Pottstown Butchers", ["Billy Beef", "Peter Pork", "Larry Lamb"], "Mr Lecter")
+  end
+
+  def test_get_team_name
+
+    response = @pottstown.team_name
+
+    assert_equal("Pottstown Butchers", response)
+
+  end
+
+  def test_get_team_roster
+
+    response = @pottstown.players
+
+    assert_equal(["Billy Beef", "Peter Pork", "Larry Lamb"], response)
+
+  end
+
+  def test_get_team_coach
+    response = @pottstown.coach
+
+    assert_equal("Mr Lecter", response)
+
+  end
+
+  def test_set_team_coach
+
+    @pottstown.coach = ("Mr Ripper")
+
+    response = @pottstown.coach
+    assert_equal("Mr Ripper", response)
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ sports_team.rb
@@ -0,0 +1,35 @@
+class SportsTeam
+  attr_accessor :team_name, :players, :coach
+
+  def initialize (team, roster, staff)
+    @team_name = team 
+    @players = roster 
+    @coach = staff
+
+  end
+
+#   def get_team_name 
+#     return @team_name
+#   end
+
+#   def get_team_roster
+#     return @players
+
+#   end
+
+# def get_team_coach
+#   return @coach
+# end
+
+# def set_team_coach(new_coach)
+# @coach = new_coach
+# end
+
+
+
+
+
+
+
+
+  end
