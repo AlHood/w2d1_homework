460d63f5c5b16c9f4b7a7297cc44dfefb2d7f789
Alastair Hood <alastair.hood85@gmail.com>
2017-01-17 09:03:47 +0000

    final

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -5,7 +5,7 @@ require_relative("../sports_team")
 class TestSports_team < Minitest::Test  
 
   def setup
-    @pottstown = SportsTeam.new("Pottstown Butchers", ["Billy Beef", "Peter Pork", "Larry Lamb"], "Mr Lecter")
+    @pottstown = SportsTeam.new("Pottstown Butchers", ["Billy Beef", "Peter Pork", "Larry Lamb"], "Mr Lecter", 0)
   end
 
   def test_get_team_name
@@ -40,6 +40,28 @@ class TestSports_team < Minitest::Test
 
   end
 
+def test_add_new_player
+
+@pottstown.add_new_player("Literally Venison")
+
+assert_equal(4, @pottstown.players.size)
+end
+
+def test_find_player
+
+response = @pottstown.find_player("Billy Beef")
+
+assert_equal("Billy Beef has been found", response)
+
+end
+
+
+def test_award_points
+
+  @pottstown.award_points(true)
+
+assert_equal(3, @pottstown.league)
+end
 
 
 
--- sports_team.rb
+++ sports_team.rb
@@ -1,35 +1,49 @@
 class SportsTeam
-  attr_accessor :team_name, :players, :coach
+  attr_accessor :team_name, :players, :coach, :league
 
-  def initialize (team, roster, staff)
+  def initialize (team, roster, staff, score)
     @team_name = team 
     @players = roster 
     @coach = staff
+    @league = score
 
   end
 
-#   def get_team_name 
-#     return @team_name
-#   end
-
-#   def get_team_roster
-#     return @players
-
-#   end
-
-# def get_team_coach
-#   return @coach
-# end
-
-# def set_team_coach(new_coach)
-# @coach = new_coach
-# end
-
-
-
-
-
-
-
+  def add_new_player(new_player)
+    @players.unshift("#{new_player}") 
 
   end
+
+  def find_player(player_name)
+    for player in @players
+      if player == player_name 
+        return "#{player_name} has been found" 
+
+      end
+    return "No. He is not here."
+
+    end
+  end
+
+def award_points(victory)
+  if victory = true
+    @league += 3
+elsif victory = false
+  @league -= 3
+
+end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
